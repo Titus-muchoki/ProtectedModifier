7744c3a43eeba068d54622263d3a72f1938d54ba
kajela <titoyut56@gmail.com>
2022-06-06 16:05:19 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,18 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
+    private String name;
+    private int number;
+    private  String object;
+    public Main() {
+        name = "tito";
+        number = 22;
+        object = "laptop";
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main myObj = new Main();
+        System.out.println(myObj.name);
+        System.out.println(myObj.number);
+        System.out.println(myObj.object);
     }
 }
